a1bf3169632ffacd220e465ed6da22ee91475103
Khoi <khoidang01234@gmail.com>
2025-10-24 04:47:50 +0700

    refactor file structure and add testing the backedn

---
+++ notes-backend/controllers/notes.js
@@ -0,0 +1,67 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+
+notesRouter.get("/", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
+
+notesRouter.get("/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.post("/", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
+});
+
+notesRouter.delete("/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+notesRouter.put("/:id", (req, res, next) => {
+  const { content, important } = req.body;
+
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (!note) {
+        return res.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        res.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = notesRouter;
